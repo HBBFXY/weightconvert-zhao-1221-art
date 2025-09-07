cae2342a5b2378a9de9de4e2e867c771a81c8a34
zhao-1221-art <2647525876@qq.com>
2025-09-07 10:57:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 weight = input('请输入重量：')
 if weight[-2:] == 'kg':
-  exchange = (eval(weight[0,-1])) * 2.2046
+  exchange = (eval(weight[0:-1])) * 2.2046
   print('对应的英制重量为｛:.3f｝磅'.format(exchange))
 elif weight[-2:] == 'pd':
-  exchange = (eval(weight[0,-1])) / 2.2046
+  exchange = (eval(weight[0:-1])) / 2.2046
   print('对应的公制重量为｛:.3f｝公斤'.format(exchange))
 else:
   print('输入错误')
